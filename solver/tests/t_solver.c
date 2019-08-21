5fe2146e5a24f5bc807c15de5d41807d2e78c61f
Henkan <hugo.lisik@epitech.eu>
2019-08-21 11:04:47 +0200

    [Add]       :   Add tests ressources and unit test
       . Add    :
                - Added unit tests to test the solve function
                - Added ressources for testing with map

--- solver/tests/t_solver.c
+++ solver/tests/t_solver.c
@@ -6,7 +6,39 @@
 */
 
 #include <criterion/criterion.h>
+#include "solver.h"
 
 Test (SOLVER, no_exit_map)
 {
+    ctrl_t *control = create_ctrl("tests/ressources/g_no_end_map.txt");
+
+    cr_assert_eq(solver(control), 1);
+}
+
+Test (SOLVER, simple_map)
+{
+    ctrl_t *control = create_ctrl("tests/ressources/g_simple_map.txt");
+
+    cr_assert_eq(solver(control), 0);
+}
+
+Test (SOLVER, complex_map)
+{
+    ctrl_t *control = create_ctrl("tests/ressources/g_complex_map.txt");
+
+    cr_assert_eq(solver(control), 0);
+}
+
+Test (SOLVER, one_line_map)
+{
+    ctrl_t *control = create_ctrl("tests/ressources/g_one_line_map.txt");
+
+    cr_assert_eq(solver(control), 0);
+}
+
+Test (SOLVER, one_col_map)
+{
+    ctrl_t *control = create_ctrl("tests/ressources/g_one_col_map.txt");
+
+    cr_assert_eq(solver(control), 0);
 }
